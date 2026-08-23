ef1230793791b468b4a071eb7f0c3b54b47e4425
Heather Spacek <heatherspacek1997@gmail.com>
2026-08-23 01:55:16 -0600

    stealing the idea from those C++ videos

---
+++ src/heatwrap/config.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class NiceListBoxConfig:
+    base_color: tuple[int] = (46, 52, 64)
+    hover_color: tuple[int] = (59, 66, 82)
+    active_color: tuple[int] = (67, 76, 94)
+    border_color: tuple[int] = (76, 86, 106)
+    panel_w: int = 250
+    panel_h: int = -1
+    item_w: int = -1
+    item_h: int = 55
--- src/heatwrap/widgets.py
+++ src/heatwrap/widgets.py
@@ -1,25 +1,15 @@
 import dearpygui.dearpygui as dpg
 
-
-class Dropdown:
-    def __init__(self, parent, tag):
-        self.parent = parent
-        self.tag = tag
-
-        with dpg.popup(parent=parent, tag=tag):
-            dpg.add_button(label="Share...")
-            dpg.add_button(label="Copy P1 only")
-            dpg.add_button(label="Copy P2 only")
-            dpg.add_button(label="Copy both players")
+from .config import NiceListBoxConfig
 
 
-# drop-down button, like for a kebab menu
-def add_dropdown(parent, tag) -> Dropdown:
-    dr = Dropdown(parent=parent, tag=tag)
-    return dr
+class Dropdown: ...
 
 
-# better listbox
-def add_biglistbox(tag):
-    with dpg.group(tag=tag):
-        ...
+class NiceListBox:
+    def __init__(self, parent, tag, config: NiceListBoxConfig = None):
+        if config is None:
+            config = NiceListBoxConfig()
+        self.theme_base = ...
+        self.theme_hovered = ...
+        self.theme_active = ...
