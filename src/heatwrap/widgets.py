92fb53532af7d530837cff05234238b3dd5acdf2
Heather Spacek <heatherspacek1997@gmail.com>
2026-09-09 20:16:57 -0600

    auto-resize for taller fonts

--- src/heatwrap/widgets.py
+++ src/heatwrap/widgets.py
@@ -128,6 +128,7 @@ class NiceListBox:
                 height=self.config.panel_h,
                 width=self.config.panel_w,
                 resizable_x=True,
+                auto_resize_y=True,
             ),
             dpg.group(horizontal=True),
         ):
